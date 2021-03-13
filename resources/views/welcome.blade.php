ca8e9cc0312fb1a20fbe0f0f4018bc449d6e4c11
pavel <prylskiy@mail.ru>
2021-03-14 00:07:13 +0700

    use include blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Welcome</h1>
+    @empty($products)
+        <div class="alert alert-warning">We don't have a products</div>
+    @else
+        <div class="row">
+            @foreach($products as $product)
+                <div class="col-3">
+                    @include('components.product-card')
+                </div>
+            @endforeach
+        </div>
+    @endif
 @endsection
